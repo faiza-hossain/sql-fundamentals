69563daf19f22ec52a0904a47ba84691616e157b
Faiza Hossain <faizahossain.tech@gmail.com>
2026-06-22 21:51:19 +0600

    Update File with Windows Partition notes

--- data-manipulation-in-sql/data_manipulation_in_sql.sql
+++ data-manipulation-in-sql/data_manipulation_in_sql.sql
@@ -482,3 +482,37 @@ GROUP BY l.name
 -- Order the query by the rank you created
 ORDER BY league_rank;
 ---- Windows Partitions-----
+SELECT
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+		 ELSE 'away' END AS warsaw_location,
+    -- Calculate separately the average home and away goals scored, partitioned by season
+    AVG(home_goal) OVER(PARTITION BY season)AS season_homeavg,
+   AVG(away_goal) OVER(PARTITION BY season) AS season_awayavg
+FROM match
+-- Filter the data set for Legia Warszawa (id 8673) matches only
+WHERE 
+	hometeam_id = '8673' 
+    OR awayteam_id = '8673' 
+ORDER BY (home_goal + away_goal) DESC;
+-----
+SELECT 
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+         ELSE 'away' END AS warsaw_location,
+	-- Calculate average goals partitioned by season and month
+    AVG(home_goal) Over(PARTITION BY season, 
+         	EXTRACT(MONTH FROM date)) AS season_mo_home,
+    AVG(away_goal) Over(PARTITION BY season, 
+            EXTRACT(MONTH FROM date)) AS season_mo_away
+FROM match
+WHERE 
+	hometeam_id = '8673'
+    OR awayteam_id = '8673'
+ORDER BY (home_goal + away_goal) DESC;
