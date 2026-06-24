3b5e329c4a40ec5f406890db97d08e743e6eaba4
Faiza Hossain <faizahossain.tech@gmail.com>
2026-06-24 22:18:34 +0600

    Update file with Sliding Windows notes

--- data-manipulation-in-sql/data_manipulation_in_sql.sql
+++ data-manipulation-in-sql/data_manipulation_in_sql.sql
@@ -516,3 +516,31 @@ WHERE
 	hometeam_id = '8673'
     OR awayteam_id = '8673'
 ORDER BY (home_goal + away_goal) DESC;
+------sliding windows-------
+SELECT 
+	date,
+	home_goal,
+	away_goal,
+    -- Create a running total and running average of home goals
+    SUM(home_goal) OVER(ORDER BY date 
+         ROWS BETWEEN Unbounded preceding AND CURRENT ROW) AS running_total,
+    AVG(home_goal) OVER(ORDER BY date 
+         ROWS BETWEEN Unbounded preceding AND CURRENT ROW) AS running_avg
+FROM match
+WHERE 
+	hometeam_id = 9908 
+	AND season = '2011/2012';
+-------
+SELECT
+    -- Select the date and away goals
+    date,
+    away_goal,
+    -- Create a running total sum and running average of away goals
+    SUM(away_goal) OVER(ORDER BY date DESC
+         ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS running_total,
+    AVG(away_goal) OVER(ORDER BY date DESC
+         ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS running_avg
+FROM match
+WHERE 
+	awayteam_id = 9908 
+    AND season = '2011/2012';
