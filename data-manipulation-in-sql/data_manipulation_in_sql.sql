2852bde01a56428218d648dc21b97d1d231cad96
Faiza Hossain <faizahossain.tech@gmail.com>
2026-06-24 22:26:49 +0600

    Update file with practice notes

--- data-manipulation-in-sql/data_manipulation_in_sql.sql
+++ data-manipulation-in-sql/data_manipulation_in_sql.sql
@@ -544,3 +544,19 @@ FROM match
 WHERE 
 	awayteam_id = 9908 
     AND season = '2011/2012';
+---------------
+SELECT 
+	m.id, 
+    t.team_long_name,
+    -- Identify matches as home/away wins or ties
+	CASE WHEN m.home_goal >m.away_goal then 'MU Win'
+		when m.home_goal <m.away_goal then 'MU Loss'
+        else 'Tie' end AS outcome
+FROM match AS m
+-- Left join team on the hometeam_ID and team_API_id
+LEFT JOIN team AS t 
+ON m.hometeam_ID = t.team_api_id
+WHERE 
+	-- Filter for 2014/2015 and Manchester United as the home team
+	m.season = '2014/2015'
+	AND t.team_long_name = 'Manchester United';
